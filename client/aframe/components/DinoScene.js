6000fe2cf75bfd2c8b960632c49dec5efbf020dc
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-05-14 14:44:58 -0700

    Add aframe and dinoScene

---
+++ client/aframe/components/DinoScene.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class DinoScene extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <a-scene>
+      </a-scene>
+    );
+  }
+};
+
+module.exports = DinoScene;
