972cfdec83006f88614eb33efc68bf40021d1528
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-05-14 16:32:36 -0700

    Remove ability to wasd

--- client/aframe/components/DinoScene.js
+++ client/aframe/components/DinoScene.js
@@ -15,7 +15,7 @@ class DinoScene extends React.Component {
       <a-sky src='/assets/prehistoric.jpg'/>
       <a-plane id='ground' material='color:brown;side:double' position='0 0 0' rotation='-90 0 0' width='100' height='100' />
       <a-entity position='0 1.5 0'>
-        <a-camera id='camera'/>
+        <a-camera id='camera' wasd-controls-enabled='false'/>
       </a-entity>
       <a-collada-model src='#asstronautDae' position='0 -0.5 0' rotation='0 180 0'/>
 
