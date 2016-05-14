60c9c033d4208619ef568e8d7969b216246f24fa
Elliot Plant <elliotaplant@gmail.com>
2016-05-14 16:52:11 -0700

    Add stall to scene

--- client/aframe/components/DinoScene.js
+++ client/aframe/components/DinoScene.js
@@ -9,7 +9,8 @@ class DinoScene extends React.Component {
     return (
       <a-scene>
       <a-assets>
-        <a-asset-item id="asstronautDae" src="/assets/John_glenn/model.dae"></a-asset-item>
+        <a-asset-item id="asstronautDae" src="/assets/John_glenn/model.dae" />
+        <a-asset-item id="stall" src="/assets/Toilet_Stall.dae" />
       </a-assets>
 
       <a-sky src='/assets/prehistoric.jpg'/>
@@ -17,11 +18,11 @@ class DinoScene extends React.Component {
       <a-entity position='0 1.5 0'>
         <a-camera id='camera' wasd-controls-enabled='false'/>
       </a-entity>
-      <a-collada-model src='#asstronautDae' position='0 -0.5 0' rotation='0 180 0'/>
+      {/*<a-collada-model src='#asstronautDae' position='0 -0.5 0' rotation='0 180 0'/>*/}
 
       <a-entity position='0 10 -10'>
         <a-plane src='/assets/ptero.png' transparent='true' width='3' height='2' rotation='90 0 0'/>
-        <a-animation 
+        <a-animation
           attribute='position'
           dur='15000'
           from='0 10 -100'
@@ -29,6 +30,7 @@ class DinoScene extends React.Component {
           repeat='indefinite'
         />
       </a-entity>
+      <a-collada-model scale="2.3 1.5 2" src='#stall' position='1.2 0 1' rotation='0 180 0'/>
 
       </a-scene>
     );
