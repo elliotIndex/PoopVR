4b7f95b34ec17f40035264897f040f516636546b
Elliot Plant <elliotaplant@gmail.com>
2016-05-14 23:41:33 -0700

    Make two circles for loading

--- client/react/components/LoadingScene.js
+++ client/react/components/LoadingScene.js
@@ -13,20 +13,24 @@ class LoadingScene extends React.Component {
     // when the assets load, bring in the component that was loading
     return (
       <a-scene>
-        <a-entity position="0 0 5">
+        <a-entity position="0 3 5">
           {colors.map((color, index) => (
             <a-entity key={index} rotation={`0 ${index*360/5} 0`}>
-              <a-sphere color={color} position='0 1 -5' />
+              <a-sphere color={color} position='0 1 -10' />
             </a-entity>
           ))}
-          <a-animation
-            begin='100'
-            attribute='rotation'
-            dur='8000'
-            from='0 0 0'
-            to='0 360 0'
-            repeat='indefinite'
-            easing='linear'
+          <a-animation begin='100' attribute='rotation' dur='8000' from='0 0 0'
+            to='0 360 0' repeat='indefinite' easing='linear'
+          />
+        </a-entity>
+        <a-entity position="0 -1 5">
+          {colors.map((color, index) => (
+            <a-entity key={index} rotation={`0 ${index*360/5} 0`}>
+              <a-sphere color={color} position='0 1 -10' />
+            </a-entity>
+          ))}
+          <a-animation begin='100' attribute='rotation' dur='8000' from='0 0 0'
+            to='0 -360 0' repeat='indefinite' easing='linear'
           />
         </a-entity>
         <a-sky color='skyblue' />
