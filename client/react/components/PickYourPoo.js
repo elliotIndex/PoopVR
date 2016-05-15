f67ec7d847d7070f131b5e5e842f90e6d075c8d4
Elliot Plant <elliotaplant@gmail.com>
2016-05-14 18:37:23 -0700

    Add click handling for each poo event

--- client/react/components/PickYourPoo.js
+++ client/react/components/PickYourPoo.js
@@ -1,24 +1,48 @@
 import React from 'react';
+import DinoScene from '../../aframe/components/DinoScene';
+import WarScene from '../../aframe/components/WarScene';
+
+const pooScenes = {
+  war: WarScene,
+  dino: DinoScene,
+}
 
 class PickYourPoo extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {};
+  }
+
+  loadPoo(poo) {
+    this.setState({ scene: pooScenes[poo] });
+  }
+
+  loadWarPoo() {
+    this.loadPoo('war');
+  }
+
+  loadDinoPoo() {
+    this.loadPoo('dino');
   }
 
   render () {
-    return (
-      <div>
-        <h1 class="title">PoopVR</h1>
-        <i class="em em-poop"></i>
-        <div class="status">
-          <p>Coming Soon!</p>
+    if (this.state.scene) {
+      return <this.state.scene />;
+    } else {
+      return (
+        <div>
+          <h1 class="title">PoopVR</h1>
+          <i class="em em-poop"></i>
+          <div class="status">
+            <p>Coming Soon!</p>
+          </div>
+          <ul>
+            <li onClick={this.loadWarPoo}>World War Two</li>
+            <li onClick={this.loadDinoPoo}>Prehistoric Poo</li>
+          </ul>
         </div>
-        <ul>
-          <li onClick={}>World War Two</li>
-          <li>Prehistoric Poo</li>
-        </ul>
-      </div>
-    );
+      );
+    }
   }
 };
 
