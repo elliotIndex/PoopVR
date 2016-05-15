312ac0acd4c146d3200e5c6d4fa1d4ae55901931
Ian deBoisblanc <iandeboisblanc@gmail.com>
2016-05-14 18:12:53 -0700

    Add siren sound to warScene

--- client/aframe/components/WarScene.js
+++ client/aframe/components/WarScene.js
@@ -10,9 +10,9 @@ class WarScene extends React.Component {
       <a-scene>
 
       <a-assets>
-        <a-asset-item id="fighterDae" src="/assets/fighterPlane/model.dae" />
-        <a-asset-item id="stall" src="/assets/Toilet_Stall.dae" />
-        <a-asset-item id="tallGrass" src="/assets/tallGrass.dae" />
+        <a-asset-item id='fighterDae' src='/assets/fighterPlane/model.dae' />
+        <a-asset-item id='stall' src='/assets/Toilet_Stall.dae' />
+        <a-asset-item id='tallGrass' src='/assets/tallGrass.dae' />
       </a-assets>
 
       <a-sky src='/assets/gloomySky.jpg' rotation='60 0 0' />
@@ -28,9 +28,9 @@ class WarScene extends React.Component {
         <a-collada-model src='#fighterDae' position='65 2 0' rotation='10 0 26'/>
         <a-collada-model src='#fighterDae' position='45 0 0' rotation='10 0 20'/>
         <a-animation
-          begin='10000' 
+          begin='5000' 
           attribute='rotation'
-          dur='5000'
+          dur='8000'
           from='0 180 0'
           to='0 -180 0'
           repeat='indefinite'
@@ -38,9 +38,13 @@ class WarScene extends React.Component {
         />
       </a-entity>
 
-      <a-collada-model scale="2.3 1.5 2" src='#tallGrass' position='-1.5 -0.8 -1.5' />
+      <a-entity id='siren' position='10 3 -15' sound='src:assets/siren.mp3;autoplay:true;loop:true;' />
 
-      <a-collada-model scale="2.3 1.5 2" src='#stall' position='1.2 0 1' rotation='0 180 0'/>
+      <a-collada-model scale='2.3 1.5 2' src='#tallGrass' position='-1.5 -0.8 -1.5' />
+
+      <a-collada-model scale='2.3 1.5 2' src='#stall' position='1.2 0 1' rotation='0 180 0'/>
+
+      <a-light type='ambient' color='#f6da80'></a-light>
 
       </a-scene>
     );
